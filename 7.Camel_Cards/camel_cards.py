cdac0e38f45acb4449c64c05cada8a7e5ede3961
Smriti Vipin Madangarli <smritivipin.madangarli2021@vitstudent.ac.in>
2023-12-07 14:23:13 +0530

    Part 2

--- 7.Camel_Cards/camel_cards.py
+++ 7.Camel_Cards/camel_cards.py
@@ -2,6 +2,8 @@ from functools import cmp_to_key
 from collections import Counter
 import re
 
+include_joker = False
+
 def get_puzzle(path):
     with open(path) as file:
         return parse_input(file)
@@ -37,6 +39,19 @@ def compare_power(card1, card2):
 
 def get_type(hand):
     frequency = Counter(hand)
+    # Checking if jokers have to be included (for part2)
+    if include_joker:
+        #Removing all joker entries and replacing them with most_common card
+        joker_count = frequency["J"]
+        del frequency["J"]
+        #If all cards are jokers, then giving them the highest value i.e 'A'
+        if not frequency:
+            frequency["A"] = 5
+        #Finding most common entry and adding the joker_count
+        else:
+            most_common, count = frequency.most_common(1)[0] #[(<entry1>:<frequency1>)]
+            frequency[most_common] += joker_count
+
     entries = len(frequency)
     if entries == 1: return "5kind"
     if entries == 2:
@@ -50,6 +65,8 @@ def get_type(hand):
 
 def compare_strength(hand1, hand2):
     VALUE = {"A":14, "K":13, "Q":12, "J":11, "T":10, "9":9, "8":8, "7":7, "6":6, "5":5, "4":4, "3":3, "2":2}
+    #If joker is included, value of J will fall down
+    if include_joker: VALUE["J"] = 1
     for char1, char2 in zip(hand1, hand2):
         if VALUE[char1] > VALUE[char2]:
             return 1
@@ -59,4 +76,10 @@ def compare_strength(hand1, hand2):
 
 path = "7.Camel_Cards\input.txt"
 CARDS = get_puzzle(path)
+
+# Part 1
+print(total_winnings(CARDS))
+
+# Part 2
+include_joker = True #including joker for part2
 print(total_winnings(CARDS))
