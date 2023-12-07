45ded82fe9541a6bcf44b1e3e5a80bd078d597a0
Smriti Vipin Madangarli <smritivipin.madangarli2021@vitstudent.ac.in>
2023-12-07 09:37:49 +0530

    Create part2_regex.py

---
+++ 1.Trebuchet/part2_regex.py
@@ -0,0 +1,21 @@
+# import re
+import regex as re
+def calibration_value(line):
+    # digits = re.findall(r'(?=(one|two|three|four|five|six|seven|eight|nine|[1-9]))', line)
+    digits = re.findall('one|two|three|four|five|six|seven|eight|nine|[1-9]', line, overlapped=True)
+    first_digit, last_digit = digit_value(digits[0]), digit_value(digits[-1])         
+    return int(first_digit + last_digit)
+
+def digit_value(element):
+    if element.isdigit(): return element
+    name2digit = {"one":"1", "two":"2", "three":"3", "four":"4", "five":"5", "six":"6", "seven":"7", "eight":"8", "nine":"9"}
+    return name2digit[element]
+
+def sum_of_calibrations(file):
+    sum = 0
+    for line in file:
+        sum += calibration_value(line)
+    return sum
+        
+with open("1.Trebuchet\input1.txt") as file:
+    print(sum_of_calibrations(file))
