f81cbf0189ae1126596642344a346c8cee5c4d42
Smriti Vipin Madangarli <smritivipin.madangarli2021@vitstudent.ac.in>
2023-12-10 13:51:02 +0530

    Part 1 cleaned up

--- 10/part1.py
+++ 10/part1.py
@@ -6,15 +6,7 @@ def get_puzzle(path):
     
 def parse_input(file):
     MAZE = [line.strip() for line in file]
-    for x in range(len(MAZE)):
-        for y in range(len(MAZE[0])):
-            if MAZE[x][y] == 'S':
-                return MAZE, (x,y)
 
-def get_farthest_point(MAZE, start):
-    loop = get_biggest_loop(MAZE, start)
-
-def get_biggest_loop(MAZE, start):
     #Pipes and their permissible neighbours
     '''
     | -> up & down, - => left & right
@@ -27,17 +19,16 @@ def get_biggest_loop(MAZE, start):
     
     adjacent = defaultdict(set)
     rows, cols = len(MAZE), len(MAZE[0])
-    print(rows, cols)
     for x in range(rows):
         for y in range(cols):
             point = MAZE[x][y]
-            if point == 'S': continue
+            if point == 'S': 
+                start = (x,y)
+                continue
             for dx, dy in neighbours[point]:
                 x_, y_ = x + dx, y + dy
                 if all([x >= 0, x < rows, y >= 0 and y < cols]):
                     adjacent[(x, y)].add((x_, y_))
-
-    print(adjacent)
 
     #Finding the adjacency list for start
     adjacent_start = set()
@@ -47,8 +38,9 @@ def get_biggest_loop(MAZE, start):
                 adjacent_start.add(u)
     adjacent[start] = adjacent_start
 
-            
+    return adjacent, start
 
+def get_farthest_point(adjacent, start):
     distance = defaultdict(lambda: float('INF'))
     queue = deque([(start, 0)])
     while queue:
@@ -58,45 +50,10 @@ def get_biggest_loop(MAZE, start):
             if distance[vertex] > current_distance:
                 queue.append((vertex, current_distance + 1))
     
-    print(max(distance.values()))
+    return max(distance.values())
 
 
     
 path = "10\input.txt"
-MAZE, start = get_puzzle(path)
-print(get_farthest_point(MAZE, start))
-
-
-# N, S, E, W = -1, +1, +1j, -1j
-# exits = {'|': (N, S), '-': (E, W),
-#          'L': (N, E), 'J': (N, W),
-#          '7': (S, W), 'F': (S, E),
-#          '.': (), 'S': (N, E, S, W)}
-
-# board = {(p:=2*i+2j*j): [p+e for e in exits[c]]
-#     for i,r in enumerate(open(path))
-#     for j,c in enumerate(r.strip())}
-# start = next(k for k,v in board.items() if len(v)==4)
-# graph = defaultdict(set)
-# for p, qs in board.items():
-#     for q in qs:
-#         graph[p].add(q)
-#         graph[q].add(p)
-# dist = defaultdict(lambda: 1_000_000)
-# q = deque([(start,0)])
-# while q:
-#     n, d = q.popleft()
-#     dist[n] = d
-#     for e in graph[n]:
-#         if dist[e] > d: q.append((e, d+1))
-
-# print(max(dist.values())//2)
-
-'''
-.....
-.F-7.
-.|.|.
-.L-J.
-.....
-
-'''
+ADJACENT, START = get_puzzle(path)
+print(get_farthest_point(ADJACENT, START))
