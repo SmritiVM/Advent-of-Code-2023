c1363c8f7819bebe3e676ba541a9b5fadf240382
Smriti Vipin Madangarli <smritivipin.madangarli2021@vitstudent.ac.in>
2023-12-23 12:22:39 +0530

    part 1 iterative (success)

--- 23/23 copy.py
+++ 23/23 copy.py
@@ -1,28 +1,24 @@
-from collections import deque
 def get_puzzle(path):
     with open(path) as file:
         return [list(line.strip()) for line in file]
 
 def get_longest_path(TRAIL):
-    paths = []
-    current_path = ""
-    rows = len(TRAIL)
-    cols = len(TRAIL[0])
-    # path = get_biggest_loop(TRAIL, (0, 1))
-    return len(find_path(TRAIL, rows, cols, current_path))
-    # find_path(0, 1, TRAIL, rows, cols, paths, current_path)
-    # max_path_length = 0
-    # for path in paths:
-    #     max_path_length = max(max_path_length, len(path))
-    # return max_path_length
-
-def find_path(TRAIL, rows, cols, current_path):
-    stack = [(0, 1, 'v')]
-    visited = {(0,1)}
+    rows, cols = len(TRAIL), len(TRAIL[0])
+    stack = [(0, 1, 0)]
+    visited = set()
+    max_length = 0
     while stack:
-        x, y, current_path = stack.pop()
+        x, y, length = stack.pop()
+        if length == -1:
+            visited.remove((x, y))
+            continue
         if x == rows - 1 and y == cols - 2:
-            return current_path
+            max_length = max(max_length, length)
+            continue
+        if (x, y) in visited: continue
+        visited.add((x, y))
+        stack.append((x, y, -1))
+
         cell = TRAIL[x][y]
         if cell == '.':
             possible_directions = ['>','<','^','v']
@@ -32,11 +28,10 @@ def find_path(TRAIL, rows, cols, current_path):
         for direction in possible_directions:
             dx, dy = DIRECTIONS[direction]
             x_, y_ = x + dx, y + dy
-            if is_valid(x_, y_, rows, cols, TRAIL) and (x_, y_) not in visited:
-                current_path += direction
-                stack.append((x_, y_, current_path))
-                visited.add((x_, y_))
-  
+            if is_valid(x_, y_, rows, cols, TRAIL):
+                stack.append((x_, y_, length + 1))
+    return max_length
+
 def is_valid(x, y, rows, cols, TRAIL):
     return (0 <= x < rows) and (0 <= y < cols) and (TRAIL[x][y] != '#')
 
@@ -44,4 +39,5 @@ def is_valid(x, y, rows, cols, TRAIL):
 path = "23\input.txt"
 TRAIL = get_puzzle(path)
 DIRECTIONS = {'>':(0, 1), '<':(0, -1), '^':(-1, 0), 'v':(1, 0)}
-# print(get_longest_path(TRAIL))
+print(get_longest_path(TRAIL))
+
