70a2940b17cc2e8b98649f6909147a8228a11f39
Smriti Vipin Madangarli <smritivipin.madangarli2021@vitstudent.ac.in>
2023-12-25 19:59:31 +0530

    part 2 success

--- 24.Never_Tell_Me_The_Odds/never_tell_me_the_odds.py
+++ 24.Never_Tell_Me_The_Odds/never_tell_me_the_odds.py
@@ -1,14 +1,16 @@
-from sympy import symbols, Eq, solve
+from sympy import symbols, Symbol, Eq, solve
 
 class Hail:
-    def __init__(self, x, y, vx, vy):
+    def __init__(self, x, y, z, vx, vy, vz):
         self.x = int(x)
         self.y = int(y)
+        self.z = int(z)
         self.vx = int(vx)
         self.vy = int(vy)
+        self.vz = int(vz)
     
     def display(self):
-        print(self.x, self.y, self.vx, self.vy)
+        print(self.x, self.y, self.z, self.vx, self.vy, self.vz)
 
 def get_puzzle(path):
     with open(path) as file:
@@ -18,9 +20,9 @@ def parse_input(file):
     HAIL = []
     for line in file:
         positions, velocities = line.strip().split(' @ ')
-        x, y = positions.split(', ')[:-1]
-        vx, vy = velocities.split(', ')[:-1]
-        HAIL.append(Hail(x, y, vx, vy))
+        x, y, z = positions.split(', ')
+        vx, vy, vz = velocities.split(', ')
+        HAIL.append(Hail(x, y, z, vx, vy, vz))
     return HAIL
 
 def get_total_intersections(HAIL, start, stop):
@@ -34,7 +36,6 @@ def get_total_intersections(HAIL, start, stop):
             if start <= x <= stop and start <= y <= stop:
                 # HAIL[i].display()
                 # HAIL[j].display()
-                # print('x', x.evalf(), y.evalf())
                 total_intersections += 1
     return total_intersections
 
@@ -57,10 +58,25 @@ def solve_equations(a1, b1, c1, a2, b2, c2):
     eq2 = Eq((a2*t1+b2*t2), -c2)
     return solve((eq1, eq2), (t1, t2))
     
+def get_rock_position(HAIL):
+    x,y,z,vx,vy,vz = symbols('x,y,z,vx,vy,vz')
+    equations = []
+    time_symbols = []
+    #Finding intersection of first 3 hailstones
+    for index, hail in enumerate(HAIL[:3]):
+        t = Symbol('t'+str(index))
+        eqx = x + vx*t - (hail.x + hail.vx*t)
+        eqy = y + vy*t - (hail.y + hail.vy*t)
+        eqz = z + vz*t - (hail.z + hail.vz*t)
 
+        equations.extend([eqx, eqy, eqz])
+        time_symbols.append(t)
+    solution = solve(equations,*([x,y,z,vx,vy,vz]+time_symbols))
+    return sum(solution[0][:3])
+    
 path = "24.Never_Tell_Me_The_Odds\input.txt"
 HAIL = get_puzzle(path)
-# for hail in HAILS:
+# for hail in HAIL:
 #     hail.display()
 #Sample input
 # print(get_total_intersections(HAIL, start=7, stop=27))
@@ -68,8 +84,11 @@ HAIL = get_puzzle(path)
 #Part 1
 print(get_total_intersections(HAIL, start=200000000000000, stop=400000000000000))
 
+#Part 2
+print(get_rock_position(HAIL))
 
 '''
+Part 1 Working
 Equation in 2 vars
 x
 19 - 2t1 = 18 - t2
